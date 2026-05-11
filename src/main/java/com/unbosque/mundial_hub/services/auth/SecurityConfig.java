ad6fa81ba79adc6d783a8bc2c229f0cfb79690c1
Daniel Rodriguez <dangelsfocus@gmail.com>
2026-05-11 10:44:51 -0500

    cors config

--- src/main/java/com/unbosque/mundial_hub/services/auth/SecurityConfig.java
+++ src/main/java/com/unbosque/mundial_hub/services/auth/SecurityConfig.java
@@ -9,16 +9,19 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource))
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session ->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -28,13 +31,11 @@ public class SecurityConfig {
                     .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                     .requestMatchers("/actuator/**").permitAll()
                     .anyRequest().authenticated()
-            );
-            /*
+            )
             .addFilterBefore(
                     jwtAuthenticationFilter,
                     UsernamePasswordAuthenticationFilter.class
             );
-            */
         return http.build();
     }
 }
