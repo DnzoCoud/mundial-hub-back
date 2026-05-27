bd965dfc0ae2cf36302ef3c942318221b4ce5be4
marianaovallos0-cmd <marianaovallos0@gmail.com>
2026-05-26 19:41:29 -0500

    add h2 to tests

--- src/test/java/com/unbosque/mundial_hub/MundialHubApplicationTests.java
+++ src/test/java/com/unbosque/mundial_hub/MundialHubApplicationTests.java
@@ -2,8 +2,10 @@ package com.unbosque.mundial_hub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MundialHubApplicationTests {
 
 	@Test
